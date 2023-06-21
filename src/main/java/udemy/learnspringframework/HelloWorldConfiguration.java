6b2a6b9e99157a316f47e7342d7e989900b27c05
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2023-06-21 17:10:02 +0900

    Spring Framework Java 구성 파일에서 자동 연결 구현

--- src/main/java/udemy/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/udemy/learnspringframework/HelloWorldConfiguration.java
@@ -3,7 +3,7 @@ package udemy.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person (String name, int age) {} // 이 두 개의 인수가 있는 Person 생성자가 자동으로 생성됨
+record Person (String name, int age, Address address) {} // 이 두 개의 인수가 있는 Person 생성자가 자동으로 생성됨
 record Address (String firstLine, String city) {}
 
 @Configuration
@@ -21,12 +21,32 @@ public class HelloWorldConfiguration {
 
     @Bean
     public Person person() {
-        return new Person("AAA", 20);
+        return new Person("AAA", 20, new Address("Main Street", "Utrecht"));
         // person.getAge 등의 메서드도 제공..
     }
 
     @Bean
+    public Person person2MethodCall() {
+        // 직접 메서드 호출해서 사용
+        return new Person(name(), age(), address());
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address2) {
+        // name, age, address2 (메서드 이름이 address 여도 빈 이름이 address2)
+        // 파라미터 값을 토대로 자동으로 사용하도록 함
+        return new Person(name, age, address2);
+    }
+
+    @Bean(name = "address2")
     public Address address() {
         return new Address("Baker Street", "London");
     }
+
+    /*
+    @Bean(name = "address3")
+    public Address address() {
+        return new Address("Motinagar", "Hyderabad");
+    }
+     */
 }

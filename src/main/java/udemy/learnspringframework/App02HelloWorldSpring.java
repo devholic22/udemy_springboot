7b612c8bbce180ab2d003fd599d969bee17d040e
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2023-06-21 16:51:43 +0900

    Spring Java 설정 파일에서 더 많은 Java Spring Bean 만들기

--- src/main/java/udemy/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/udemy/learnspringframework/App02HelloWorldSpring.java
@@ -10,6 +10,13 @@ public class App02HelloWorldSpring {
                 new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 
         // 2: Configure the things that we want Spring to manage
+        // HelloWorldConfiguration - @Configuration
+        // name - @Bean
+
+        // 3. Retrieving Beans managed by Spring
         System.out.println(context.getBean("name"));
+        System.out.println(context.getBean("age"));
+        System.out.println(context.getBean("person"));
+        System.out.println(context.getBean("address"));
     }
 }
--- src/main/java/udemy/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/udemy/learnspringframework/HelloWorldConfiguration.java
@@ -3,11 +3,30 @@ package udemy.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person (String name, int age) {} // 이 두 개의 인수가 있는 Person 생성자가 자동으로 생성됨
+record Address (String firstLine, String city) {}
+
 @Configuration
 public class HelloWorldConfiguration {
 
     @Bean
     public String name() {
         return "Hyunjoon";
+    }
+
+    @Bean
+    public int age() {
+        return 24;
+    }
+
+    @Bean
+    public Person person() {
+        return new Person("AAA", 20);
+        // person.getAge 등의 메서드도 제공..
+    }
+
+    @Bean
+    public Address address() {
+        return new Address("Baker Street", "London");
     }
 }
